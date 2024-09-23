f9e34cf47a78fdd24fccaf66a76fc8117d720fd7
Schild, Philippe <Philippe.Schild@adesso.de>
2024-09-23 21:38:34 +0200

    api generations

---
+++ src/app/api/openapi/model/models.ts
@@ -0,0 +1,6 @@
+export * from './createPlayerDto';
+export * from './gameOverviewDto';
+export * from './penaltyDto';
+export * from './placeDto';
+export * from './playerDto';
+export * from './updatePlayerDto';
